cc47ddc1a5785a5b995be5afab95ccc13b1cec6e
Andrew Jones <drjones@redhat.com>
2015-02-01 19:34:29 +0100

    x86: expose spin_lock/unlock to lib code
    Move the spin_lock/unlock declarations to lib/x86/asm/spinlock.h,
    allowing lib code, e.g. lib/report.c, to use spinlocks.
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- lib/x86/smp.h
+++ lib/x86/smp.h
@@ -1,13 +1,10 @@
 #ifndef __SMP_H
 #define __SMP_H
+#include <asm/spinlock.h>
 
 #define mb() 	asm volatile("mfence":::"memory")
 #define rmb()	asm volatile("lfence":::"memory")
 #define wmb()	asm volatile("sfence" ::: "memory")
-
-struct spinlock {
-    int v;
-};
 
 void smp_init(void);
 
@@ -15,7 +12,5 @@ int cpu_count(void);
 int smp_id(void);
 void on_cpu(int cpu, void (*function)(void *data), void *data);
 void on_cpu_async(int cpu, void (*function)(void *data), void *data);
-void spin_lock(struct spinlock *lock);
-void spin_unlock(struct spinlock *lock);
 
 #endif
