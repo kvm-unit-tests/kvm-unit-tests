e65bcc6253c001a11469026b8689918ea929a531
Andrew Jones <drjones@redhat.com>
2015-07-17 16:15:52 +0200

    powerpc/ppc64: add hcall support and putchar
    Add broken sc1 detection and patching and an hcall for putchar,
    to use in puts. That, along with a couple more lines in start to
    prepare for C code, and a branch to main(), gets us "hello world".
    Run with
    
    qemu-system-ppc64 -M pseries            \
            -bios powerpc/boot_rom.bin      \
            -display none -serial stdio     \
            -kernel powerpc/selftest.elf
    
    (We're still not relocating yet, that comes in a later patch. Thus,
     testing hello-world at this point requires a hacked QEMU and linking
     the unit test at QEMU's kernel load address.)
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Tested-by: Laurent Vivier <lvivier@redhat.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

--- lib/powerpc/io.c
+++ lib/powerpc/io.c
@@ -6,15 +6,26 @@
  * This work is licensed under the terms of the GNU LGPL, version 2.
  */
 #include <libcflat.h>
+#include <asm/spinlock.h>
+
+extern void halt(int code);
+extern void putchar(int c);
+
+static struct spinlock print_lock;
 
 void io_init(void)
 {
 }
 
-void puts(const char *s __unused)
+void puts(const char *s)
 {
+	spin_lock(&print_lock);
+	while (*s)
+		putchar(*s++);
+	spin_unlock(&print_lock);
 }
 
-void exit(int code __unused)
+void exit(int code)
 {
+	halt(code);
 }
