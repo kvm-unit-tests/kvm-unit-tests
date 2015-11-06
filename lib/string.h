cc80e7ce46406ed62153f9859210803f23966748
Andrew Jones <drjones@redhat.com>
2015-11-05 18:24:29 -0600

    trivial: lib: missing extern in string.h
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Message-Id: <1446769483-21586-5-git-send-email-drjones@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- lib/string.h
+++ lib/string.h
@@ -6,7 +6,7 @@ extern char *strcat(char *dest, const char *src);
 extern char *strcpy(char *dest, const char *src);
 extern int strcmp(const char *a, const char *b);
 extern char *strchr(const char *s, int c);
-char *strstr(const char *haystack, const char *needle);
+extern char *strstr(const char *haystack, const char *needle);
 extern void *memset(void *s, int c, size_t n);
 extern void *memcpy(void *dest, const void *src, size_t n);
 extern int memcmp(const void *s1, const void *s2, size_t n);
