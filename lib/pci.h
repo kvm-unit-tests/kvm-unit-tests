fa80a74d0ad1ebf060cbc37cd22ee791fd7e95f8
Alexander Gordeev <agordeev@redhat.com>
2016-11-07 11:14:35 +0100

    pci: Add 'extern' to public function declarations
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Andrew Jones <drjones@redhat.com>
    Cc: Peter Xu <peterx@redhat.com>
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>
    Message-Id: <24c5c29c3bbd3fed9091efa8586c90068037f168.1478512824.git.agordeev@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- lib/pci.h
+++ lib/pci.h
@@ -15,10 +15,10 @@ enum {
 	PCIDEVADDR_INVALID = 0xffff,
 };
 
-pcidevaddr_t pci_find_dev(uint16_t vendor_id, uint16_t device_id);
-unsigned long pci_bar_addr(pcidevaddr_t dev, int bar_num);
-bool pci_bar_is_memory(pcidevaddr_t dev, int bar_num);
-bool pci_bar_is_valid(pcidevaddr_t dev, int bar_num);
+extern pcidevaddr_t pci_find_dev(uint16_t vendor_id, uint16_t device_id);
+extern unsigned long pci_bar_addr(pcidevaddr_t dev, int bar_num);
+extern bool pci_bar_is_memory(pcidevaddr_t dev, int bar_num);
+extern bool pci_bar_is_valid(pcidevaddr_t dev, int bar_num);
 
 /*
  * pci-testdev is a driver for the pci-testdev qemu pci device. The
