c9f026ae672067703374161c669f4d5f6ea41048
Peter Xu <peterx@redhat.com>
2017-01-12 14:24:57 +0100

    run_tests: allow run tests in parallel
    run_task.sh is getting slow. This patch is trying to make it faster by
    running the tests concurrently.
    
    We provide a new parameter "-j" for the run_tests.sh, which can be used
    to specify how many run queues we want for the tests. Default queue
    length is 1, which is the old behavior.
    
    Quick test on my laptop (4 cores, 2 threads each) shows 3x speed boost:
    
       |-----------------+-----------|
       | command         | time used |
       |-----------------+-----------|
       | run_test.sh     | 75s       |
       | run_test.sh -j8 | 27s       |
       |-----------------+-----------|
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    [Moved code to run_tests.bash, make "wait -n" support optional if -j1. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- run_tests.sh
+++ run_tests.sh
@@ -14,9 +14,11 @@ function usage()
 cat <<EOF
 
 Usage: $0 [-g group] [-h] [-v]
+Usage: $0 [-g group] [-h] [-v] [-j NUM-TASKS]
 
     -g: Only execute tests in the given group
     -h: Output this help text
+    -j: Execute tests in parallel
     -v: Enables verbose mode
 
 Set the environment variable QEMU=/path/to/qemu-system-ARCH to
@@ -28,7 +30,7 @@ EOF
 RUNTIME_arch_run="./$TEST_DIR/run"
 source scripts/runtime.bash
 
-while getopts "g:hv" opt; do
+while getopts "g:hj:v" opt; do
     case $opt in
         g)
             only_group=$OPTARG
@@ -36,6 +38,13 @@ while getopts "g:hv" opt; do
         h)
             usage
             exit
+            ;;
+        j)
+            unittest_run_queues=$OPTARG
+            if (( $unittest_run_queues <= 0 )); then
+                echo "Invalid -j option: $unittest_run_queues"
+                exit 2
+            fi
             ;;
         v)
             verbose="yes"
@@ -60,11 +69,21 @@ function run_task()
 {
 	local testname="$1"
 
+	while (( $(jobs | wc -l) == $unittest_run_queues )); do
+		# wait for any background test to finish
+		wait -n 2>/dev/null
+	done
+
 	RUNTIME_log_file="${unittest_log_dir}/${testname}.log"
-	run "$@"
+	if [ $unittest_run_queues = 1 ]; then
+		run "$@"
+	else
+		run "$@" &
+	fi
 }
 
 : ${unittest_log_dir:=logs}
+: ${unittest_run_queues:=1}
 config=$TEST_DIR/unittests.cfg
 
 rm -rf $unittest_log_dir.old
@@ -73,4 +92,8 @@ mkdir $unittest_log_dir || exit 2
 
 echo "BUILD_HEAD=$(cat build-head)" > $unittest_log_dir/SUMMARY
 
+trap "wait; exit 130" SIGINT
 for_each_unittest $config run_task
+
+# wait until all tasks finish
+wait
