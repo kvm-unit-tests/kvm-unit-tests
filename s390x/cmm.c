42df42916ef14e8255eb4c6457f9a59468597e39
Janosch Frank <frankja@linux.vnet.ibm.com>
2018-03-27 13:55:18 +0200

    s390x: Add cmm tests
    Collaborative Memory Management is the extended vm memory usage
    hinting for the hypervisor and handling the ESSA instruction is quite
    complicated. Let's add at least a bare-bones test, maybe someone will
    extend it later.
    
    Signed-off-by: Janosch Frank <frankja@linux.vnet.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

---
+++ s390x/cmm.c
@@ -0,0 +1,69 @@
+/*
+ * CMM tests (ESSA)
+ *
+ * Copyright (c) 2018 IBM Corp
+ *
+ * Authors:
+ *  Janosch Frank <frankja@linux.vnet.ibm.com>
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License version 2.
+ */
+
+#include <libcflat.h>
+#include <asm/asm-offsets.h>
+#include <asm/interrupt.h>
+#include <asm/page.h>
+
+static uint8_t pagebuf[PAGE_SIZE] __attribute__((aligned(PAGE_SIZE)));
+
+static unsigned long essa(uint8_t state, unsigned long paddr)
+{
+	uint64_t extr_state;
+
+	asm volatile(".insn rrf,0xb9ab0000,%[extr_state],%[addr],%[new_state],0"
+			: [extr_state] "=d" (extr_state)
+			: [addr] "a" (paddr), [new_state] "i" (state));
+	return (unsigned long)extr_state;
+}
+
+static void test_params(void)
+{
+	expect_pgm_int();
+	essa(8, (unsigned long)pagebuf);
+	check_pgm_int_code(PGM_INT_CODE_SPECIFICATION);
+}
+
+static void test_priv(void)
+{
+	expect_pgm_int();
+	enter_pstate();
+	essa(0, (unsigned long)pagebuf);
+	check_pgm_int_code(PGM_INT_CODE_PRIVILEGED_OPERATION);
+}
+
+/* Unfortunately the availability is not indicated by stfl bits, but
+ * we have to try to execute it and test for an operation exception.
+ */
+static bool test_availability(void)
+{
+	expect_pgm_int();
+	essa(0, (unsigned long)pagebuf);
+	return clear_pgm_int() == 0;
+}
+
+int main(void)
+{
+	bool has_essa = test_availability();
+
+	report_prefix_push("cmm");
+	report_xfail("ESSA available", !has_essa, has_essa);
+	if (!has_essa)
+		goto done;
+
+	test_priv();
+	test_params();
+done:
+	report_prefix_pop();
+	return report_summary();
+}
