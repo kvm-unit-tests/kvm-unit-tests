0af4b381856eb3e82bcfeac5a2fef73828537a7b
Janosch Frank <frankja@linux.ibm.com>
2019-01-18 12:41:54 +0100

    s390x: Add Sigp header file
    The magic numbers for SIGP are not nice to read, so let's put them in
    a new header file. Also move sigp_stop() there, it will later be
    joined by more sigp functions and definitions.
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- lib/s390x/io.c
+++ lib/s390x/io.c
@@ -14,6 +14,7 @@
 #include <argv.h>
 #include <asm/spinlock.h>
 #include <asm/facility.h>
+#include <asm/sigp.h>
 #include "sclp.h"
 
 extern char ipl_args[];
@@ -28,16 +29,6 @@ void puts(const char *s)
 	spin_lock(&lock);
 	sclp_print(s);
 	spin_unlock(&lock);
-}
-
-static void sigp_stop(void)
-{
-	register unsigned long status asm ("1") = 0;
-	register unsigned long cpu asm ("2") = 0;
-
-	asm volatile(
-		"	sigp %0,%1,0(%2)\n"
-		: "+d" (status)  : "d" (cpu), "d" (5) : "cc");
 }
 
 void setup(void)
