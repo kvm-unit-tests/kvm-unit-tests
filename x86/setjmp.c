6f6a3713888144c9ac51e0438aa54bb1c5f3af45
Bill Wendling <morbo@google.com>
2019-09-19 15:14:53 -0700

    x86: setjmp: check expected value of "i" to give better feedback
    Use a list of expected values instead of printing out numbers, which
    aren't very meaningful. This prints only if the expected and actual
    values differ.
    
    Signed-off-by: Bill Wendling <morbo@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- x86/setjmp.c
+++ x86/setjmp.c
@@ -1,19 +1,26 @@
 #include "libcflat.h"
 #include "setjmp.h"
 
+static const int expected[] = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+};
+
+#define NUM_LONGJMPS ARRAY_SIZE(expected)
+
 int main(void)
 {
-    volatile int i;
+    volatile int index = 0;
     jmp_buf j;
+    int i;
 
-    if (setjmp(j) == 0) {
-	    i = 0;
+    i = setjmp(j);
+    if (expected[index] != i) {
+	    printf("FAIL: actual %d / expected %d\n", i, expected[index]);
+	    return -1;
     }
-    printf("%d\n", i);
-    if (++i < 10) {
-	    longjmp(j, 1);
-    }
+    index++;
+    if (i + 1 < NUM_LONGJMPS)
+	    longjmp(j, i + 1);
 
-    printf("done\n");
     return 0;
 }
