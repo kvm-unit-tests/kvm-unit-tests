b2402d33d42b092d65845fc956e3cdbe01628eca
Paolo Bonzini <pbonzini@redhat.com>
2020-02-28 12:11:04 +0100

    pci: use uint32_t for unsigned long values
    The "pci_bar_*" functions use 64-bit masks, but the results are assigned
    to 32-bit variables; clang complains. Use signed masks that can be
    sign-extended at will.
    
    Reported-by: Bill Wendling <morbo@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- lib/linux/pci_regs.h
+++ lib/linux/pci_regs.h
@@ -96,8 +96,8 @@
 #define  PCI_BASE_ADDRESS_MEM_TYPE_1M	0x02	/* Below 1M [obsolete] */
 #define  PCI_BASE_ADDRESS_MEM_TYPE_64	0x04	/* 64 bit address */
 #define  PCI_BASE_ADDRESS_MEM_PREFETCH	0x08	/* prefetchable? */
-#define  PCI_BASE_ADDRESS_MEM_MASK	(~0x0fUL)
-#define  PCI_BASE_ADDRESS_IO_MASK	(~0x03UL)
+#define  PCI_BASE_ADDRESS_MEM_MASK	(~0x0f)
+#define  PCI_BASE_ADDRESS_IO_MASK	(~0x03)
 /* bit 1 is reserved if address_space = 1 */
 
 /* Header type 0 (normal devices) */
