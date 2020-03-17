kvm-unit-tests/kvm-unit-tests

commit b2a1ee7ea179e24f2da1124f763bb0a8f0e03b01

x86: ioapic: Test physical and logical destination mode
This patch tests the physical destination mode by sending an
interrupt to one of the vcpus and logical destination mode by
sending an interrupt to more than one vcpus.

Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/ioapic.c
@@ -405,12 +405,73 @@ static void test_ioapic_self_reconfigure(void)
 	report("Reconfigure self", g_isr_84 == 1 && e.remote_irr == 0);
 }
 
+static volatile int g_isr_85;
+
+static void ioapic_isr_85(isr_regs_t *regs)
+{
+	++g_isr_85;
+	set_irq_line(0x0e, 0);
+	eoi();
+}
+
+static void test_ioapic_physical_destination_mode(void)
+{
+	ioapic_redir_entry_t e = {
+		.vector = 0x85,
+		.delivery_mode = 0,
+		.dest_mode = 0,
+		.dest_id = 0x1,
+		.trig_mode = TRIGGER_LEVEL,
+	};
+	handle_irq(0x85, ioapic_isr_85);
+	ioapic_write_redir(0xe, e);
+	set_irq_line(0x0e, 1);
+	do {
+		pause();
+	} while(g_isr_85 != 1);
+	report("ioapic physical destination mode", g_isr_85 == 1);
+}
+
+static volatile int g_isr_86;
+
+static void ioapic_isr_86(isr_regs_t *regs)
+{
+	++g_isr_86;
+	set_irq_line(0x0e, 0);
+	eoi();
+}
+
+static void test_ioapic_logical_destination_mode(void)
+{
+	/* Number of vcpus which are configured/set in dest_id */
+	int nr_vcpus = 3;
+	ioapic_redir_entry_t e = {
+		.vector = 0x86,
+		.delivery_mode = 0,
+		.dest_mode = 1,
+		.dest_id = 0xd,
+		.trig_mode = TRIGGER_LEVEL,
+	};
+	handle_irq(0x86, ioapic_isr_86);
+	ioapic_write_redir(0xe, e);
+	set_irq_line(0x0e, 1);
+	do {
+		pause();
+	} while(g_isr_86 < nr_vcpus);
+	report("ioapic logical destination mode", g_isr_86 == nr_vcpus);
+}
+
+static void update_cr3(void *cr3)
+{
+	write_cr3((ulong)cr3);
+}
 
 int main(void)
 {
 	setup_vm();
 	smp_init();
 
+	on_cpus(update_cr3, (void *)read_cr3());
 	mask_pic_interrupts();
 
 	if (enable_x2apic())
@@ -448,7 +509,11 @@ int main(void)
 		test_ioapic_edge_tmr_smp(true);
 
 		test_ioapic_self_reconfigure();
+		test_ioapic_physical_destination_mode();
 	}
+
+	if (cpu_count() > 3)
+		test_ioapic_logical_destination_mode();
 
 	return report_summary();
 }

commit dcf27dc5b5499bd35e5a19b68cf4fc692a73e796

x86: Fix the logical destination mode test
There are following issues with the ioapic logical destination mode test:

- A race condition that is triggered when the interrupt handler
  ioapic_isr_86() is called at the same time by multiple vCPUs. Due to this
  the g_isr_86 is not correctly incremented. To prevent this a spinlock is
  added around ‘g_isr_86++’.

- On older QEMU versions initial x2APIC ID is not set, that is why
  the local APIC IDs of each vCPUs are not configured. Hence the logical
  destination mode test fails/hangs. Adding ‘+x2apic’ to the qemu -cpu params
  ensures that the local APICs are configured every time, irrespective of the
  QEMU version.

- With ‘-machine kernel_irqchip=split’ included in the ioapic test
  test_ioapic_self_reconfigure() always fails and somehow leads to a state where
  after submitting IOAPIC fixed delivery - logical destination mode request we
  never receive an interrupt back. For now, the physical and logical destination
  mode tests are moved above test_ioapic_self_reconfigure().

Fixes: b2a1ee7e ("kvm-unit-test: x86: ioapic: Test physical and logical destination mode")
Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/ioapic.c
@@ -432,10 +432,13 @@ static void test_ioapic_physical_destination_mode(void)
 }
 
 static volatile int g_isr_86;
+struct spinlock ioapic_lock;
 
 static void ioapic_isr_86(isr_regs_t *regs)
 {
+	spin_lock(&ioapic_lock);
 	++g_isr_86;
+	spin_unlock(&ioapic_lock);
 	set_irq_line(0x0e, 0);
 	eoi();
 }
@@ -501,6 +504,10 @@ int main(void)
 	test_ioapic_level_tmr(true);
 	test_ioapic_edge_tmr(true);
 
+	test_ioapic_physical_destination_mode();
+	if (cpu_count() > 3)
+		test_ioapic_logical_destination_mode();
+
 	if (cpu_count() > 1) {
 		test_ioapic_edge_tmr_smp(false);
 		test_ioapic_level_tmr_smp(false);
@@ -508,11 +515,7 @@ int main(void)
 		test_ioapic_edge_tmr_smp(true);
 
 		test_ioapic_self_reconfigure();
-		test_ioapic_physical_destination_mode();
 	}
-
-	if (cpu_count() > 3)
-		test_ioapic_logical_destination_mode();
 
 	return report_summary();
 }

x86/unittests.cfg
@@ -46,7 +46,7 @@ timeout = 30
 [ioapic]
 file = ioapic.flat
 smp = 4
-extra_params = -cpu qemu64
+extra_params = -cpu qemu64,+x2apic
 arch = x86_64
 
 [cmpxchg8b]