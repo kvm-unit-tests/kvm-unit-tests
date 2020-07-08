59b243dcd58e625a019e544c791986f25cbbf93c
Thomas Huth <thuth@redhat.com>
2020-07-08 17:00:25 +0200

    s390x/cpumodel: The missing DFP facility on TCG is expected
    When running the kvm-unit-tests with TCG on s390x, the cpumodel test
    always reports the error about the missing DFP (decimal floating point)
    facility. This is kind of expected, since DFP is not required for
    running Linux and thus nobody is really interested in implementing
    this facility in TCG. Thus let's mark this as an expected error instead,
    so that we can run the kvm-unit-tests also with TCG without getting
    test failures that we do not care about.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Janosch Frank <frankja@linux.ibm.com>
    Message-Id: <20200708150025.20631-1-thuth@redhat.com>
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>

---
+++ lib/s390x/vm.h
@@ -0,0 +1,14 @@
+/*
+ * Functions to retrieve VM-specific information
+ *
+ * Copyright (c) 2020 Red Hat Inc
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef S390X_VM_H
+#define S390X_VM_H
+
+bool vm_is_tcg(void);
+
+#endif  /* S390X_VM_H */
