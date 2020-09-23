238b5967059e9dba6f64ee2b635f2d363ad4abb4
Marc Hartmayer <mhartmay@linux.ibm.com>
2020-09-23 15:47:58 +0200

    s390x: add Protected VM support
    Add support for Protected Virtual Machine (PVM) tests. For starting a
    PVM guest we must be able to generate a PVM image by using the
    `genprotimg` tool from the s390-tools collection. This requires the
    ability to pass a machine-specific host-key document, so the option
    `--host-key-document` is added to the configure script.
    
    Reviewed-by: Janosch Frank <frankja@linux.ibm.com>
    Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>
    Message-Id: <20200923134758.19354-5-mhartmay@linux.ibm.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    [thuth: Drop the verbose "SKIP ... (no host-key document specified)" output]
    Signed-off-by: Thomas Huth <thuth@redhat.com>

---
+++ scripts/s390x/func.bash
@@ -0,0 +1,35 @@
+# The file scripts/common.bash has to be the only file sourcing this
+# arch helper file
+source config.mak
+
+ARCH_CMD=arch_cmd_s390x
+
+function arch_cmd_s390x()
+{
+	local cmd=$1
+	local testname=$2
+	local groups=$3
+	local smp=$4
+	local kernel=$5
+	local opts=$6
+	local arch=$7
+	local check=$8
+	local accel=$9
+	local timeout=${10}
+
+	# run the normal test case
+	"$cmd" "$testname" "$groups" "$smp" "$kernel" "$opts" "$arch" "$check" "$accel" "$timeout"
+
+	# run PV test case
+	kernel=${kernel%.elf}.pv.bin
+	testname=${testname}_PV
+	if [ ! -f "${kernel}" ]; then
+		if [ -z "${HOST_KEY_DOCUMENT}" ]; then
+			return 2
+		fi
+
+		print_result 'SKIP' $testname '' 'PVM image was not created'
+		return 2
+	fi
+	"$cmd" "$testname" "$groups pv" "$smp" "$kernel" "$opts" "$arch" "$check" "$accel" "$timeout"
+}
