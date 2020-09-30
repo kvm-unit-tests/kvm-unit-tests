cfe952392633610e20229178be76ff8b0a8f3b6a
Sean Christopherson <sean.j.christopherson@intel.com>
2020-09-29 21:34:36 -0700

    x86: Add one-off test to verify setting LA57 fails when it's unsupported
    Add an i386-only test to check that setting CR4.LA57 fails when 5-level
    paging is not exposed to the guest.  Old versions of KVM don't intercept
    LA57 by default on VMX, which means a clever guest could set LA57
    without it being detected by KVM.
    
    This test is i386-only because toggling CR4.LA57 in long mode is
    illegal, i.e. won't verify the desired KVM behavior.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200930043436.29270-1-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ x86/la57.c
@@ -0,0 +1,13 @@
+#include "libcflat.h"
+#include "processor.h"
+#include "desc.h"
+
+int main(int ac, char **av)
+{
+	int vector = write_cr4_checking(read_cr4() | X86_CR4_LA57);
+	int expected = this_cpu_has(X86_FEATURE_LA57) ? 0 : 13;
+
+	report(vector == expected, "%s when CR4.LA57 %ssupported",
+	       expected ? "#GP" : "No fault", expected ? "un" : "");
+	return report_summary();
+}
