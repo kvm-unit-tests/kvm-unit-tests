132f6f18c3dadce78702704f402679cdcdce944d
Thomas Huth <thuth@redhat.com>
2020-10-01 09:22:31 +0200

    kbuild: fix asm-offset generation to work with clang
    KBuild abuses the asm statement to write to a file and
    clang chokes about these invalid asm statements. Hack it
    even more by fooling this is actual valid asm code.
    
    This is an adaption of the Linux kernel commit cf0c3e68aa81f992b0
    which in turn is based on a patch for the U-Boot:
      http://patchwork.ozlabs.org/patch/375026/
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20201001072234.143703-5-thuth@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Tested-by: Andrew Jones <drjones@redhat.com>

--- lib/kbuild.h
+++ lib/kbuild.h
@@ -1,8 +1,8 @@
 #ifndef _KBUILD_H_
 #define _KBUILD_H_
 #define DEFINE(sym, val) \
-	asm volatile("\n->" #sym " %0 " #val : : "i" (val))
+	asm volatile("\n.ascii \"->" #sym " %0 " #val "\"" : : "i" (val))
 #define OFFSET(sym, str, mem)	DEFINE(sym, offsetof(struct str, mem))
-#define COMMENT(x)		asm volatile("\n->#" x)
-#define BLANK()			asm volatile("\n->" : : )
+#define COMMENT(x)		asm volatile("\n.ascii \"->#" x "\"")
+#define BLANK()			asm volatile("\n.ascii \"->\"" : : )
 #endif
