68200001515097e4c58c35770dadcaee0e5b79ef
Janosch Frank <frankja@linux.ibm.com>
2021-05-20 07:44:38 +0000

    s390x: mvpg: Add SIE mvpg test
    Let's also check the PEI values to make sure our VSIE implementation
    is correct.
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Acked-by: Thomas Huth <thuth@redhat.com>

---
+++ s390x/snippets/c/mvpg-snippet.c
@@ -0,0 +1,33 @@
+#include <libcflat.h>
+
+static inline void force_exit(void)
+{
+	asm volatile("	diag	0,0,0x44\n");
+}
+
+static inline int mvpg(unsigned long r0, void *dest, void *src)
+{
+	register unsigned long reg0 asm ("0") = r0;
+	int cc;
+
+	asm volatile("	mvpg    %1,%2\n"
+		     "	ipm     %0\n"
+		     "	srl     %0,28"
+		     : "=d" (cc) : "a" (dest), "a" (src), "d" (reg0)
+		     : "memory", "cc");
+	return cc;
+}
+
+static void test_mvpg_real(void)
+{
+	mvpg(0, (void *)0x5000, (void *)0x6000);
+	force_exit();
+}
+
+__attribute__((section(".text"))) int main(void)
+{
+	test_mvpg_real();
+	test_mvpg_real();
+	test_mvpg_real();
+	return 0;
+}
