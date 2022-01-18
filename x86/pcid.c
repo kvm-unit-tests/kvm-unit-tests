kvm-unit-tests/kvm-unit-tests

commit b44d84dae10c1c835e2bb8e8ec4a308dea869c1e

Add PCID/INVPCID test
Add unit test for basic functionality of PCID/INVPCID feature exposing in kvm.

Signed-off-by: Junjie Mao <junjie.mao@intel.com>
Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

x86/pcid.c
@@ -1,0 +1,185 @@
+/* Basic PCID & INVPCID functionality test */
+
+#include "libcflat.h"
+#include "processor.h"
+#include "desc.h"
+
+int nr_passed, nr_tests;
+
+#define X86_FEATURE_PCID       (1 << 17)
+#define X86_FEATURE_INVPCID    (1 << 10)
+
+#define X86_CR0_PG             (1 << 31)
+#define X86_CR3_PCID_MASK      0x00000fff
+#define X86_CR4_PCIDE          (1 << 17)
+
+#define X86_IA32_EFER          0xc0000080
+#define X86_EFER_LMA           (1UL << 8)
+
+struct invpcid_desc {
+    unsigned long pcid : 12;
+    unsigned long rsv  : 52;
+    unsigned long addr : 64;
+};
+
+static void report(const char *name, int passed)
+{
+    ++nr_tests;
+    if (passed)
+        ++nr_passed;
+    printf("%s: %s\n", name, passed ? "PASS" : "FAIL");
+}
+
+int write_cr0_checking(unsigned long val)
+{
+    asm volatile(ASM_TRY("1f")
+                 "mov %0, %%cr0\n\t"
+                 "1:": : "r" (val));
+    return exception_vector();
+}
+
+int write_cr4_checking(unsigned long val)
+{
+    asm volatile(ASM_TRY("1f")
+                 "mov %0, %%cr4\n\t"
+                 "1:": : "r" (val));
+    return exception_vector();
+}
+
+int invpcid_checking(unsigned long type, void *desc)
+{
+    asm volatile (ASM_TRY("1f")
+                  ".byte 0x66,0x0f,0x38,0x82,0x18 \n\t" /* invpcid (%rax), %rbx */
+                  "1:" : : "a" (desc), "b" (type));
+    return exception_vector();
+}
+
+void test_cpuid_consistency(int pcid_enabled, int invpcid_enabled)
+{
+    int passed = !(!pcid_enabled && invpcid_enabled);
+    report("CPUID consistency", passed);
+}
+
+void test_pcid_enabled(void)
+{
+    int passed = 0;
+    ulong cr0 = read_cr0(), cr3 = read_cr3(), cr4 = read_cr4();
+
+    /* try setting CR4.PCIDE, no exception expected */
+    if (write_cr4_checking(cr4 | X86_CR4_PCIDE) != 0)
+        goto report;
+
+    /* try clearing CR0.PG when CR4.PCIDE=1, #GP expected */
+    if (write_cr0_checking(cr0 | X86_CR0_PG) != GP_VECTOR)
+        goto report;
+
+    write_cr4(cr4);
+
+    /* try setting CR4.PCIDE when CR3[11:0] != 0 , #GP expected */
+    write_cr3(cr3 | 0x001);
+    if (write_cr4_checking(cr4 | X86_CR4_PCIDE) != GP_VECTOR)
+        goto report;
+
+    passed = 1;
+
+report:
+    report("Test on PCID when enabled", passed);
+}
+
+void test_pcid_disabled(void)
+{
+    int passed = 0;
+    ulong cr4 = read_cr4();
+
+    /* try setting CR4.PCIDE, #GP expected */
+    if (write_cr4_checking(cr4 | X86_CR4_PCIDE) != GP_VECTOR)
+        goto report;
+
+    passed = 1;
+
+report:
+    report("Test on PCID when disabled", passed);
+}
+
+void test_invpcid_enabled(void)
+{
+    int passed = 0;
+    ulong cr4 = read_cr4();
+    struct invpcid_desc desc;
+    desc.rsv = 0;
+
+    /* try executing invpcid when CR4.PCIDE=0, desc.pcid=0 and type=1
+     * no exception expected
+     */
+    desc.pcid = 0;
+    if (invpcid_checking(1, &desc) != 0)
+        goto report;
+
+    /* try executing invpcid when CR4.PCIDE=0, desc.pcid=1 and type=1
+     * #GP expected
+     */
+    desc.pcid = 1;
+    if (invpcid_checking(1, &desc) != GP_VECTOR)
+        goto report;
+
+    if (write_cr4_checking(cr4 | X86_CR4_PCIDE) != 0)
+        goto report;
+
+    /* try executing invpcid when CR4.PCIDE=1
+     * no exception expected
+     */
+    desc.pcid = 10;
+    if (invpcid_checking(2, &desc) != 0)
+        goto report;
+
+    passed = 1;
+
+report:
+    report("Test on INVPCID when enabled", passed);
+}
+
+void test_invpcid_disabled(void)
+{
+    int passed = 0;
+    struct invpcid_desc desc;
+
+    /* try executing invpcid, #UD expected */
+    if (invpcid_checking(2, &desc) != UD_VECTOR)
+        goto report;
+
+    passed = 1;
+
+report:
+    report("Test on INVPCID when disabled", passed);
+}
+
+int main(int ac, char **av)
+{
+    struct cpuid _cpuid;
+    int pcid_enabled = 0, invpcid_enabled = 0;
+
+    setup_idt();
+
+    _cpuid = cpuid(1);
+    if (_cpuid.c & X86_FEATURE_PCID)
+        pcid_enabled = 1;
+    _cpuid = cpuid_indexed(7, 0);
+    if (_cpuid.b & X86_FEATURE_INVPCID)
+        invpcid_enabled = 1;
+
+    test_cpuid_consistency(pcid_enabled, invpcid_enabled);
+
+    if (pcid_enabled)
+        test_pcid_enabled();
+    else
+        test_pcid_disabled();
+
+    if (invpcid_enabled)
+        test_invpcid_enabled();
+    else
+        test_invpcid_disabled();
+
+    printf("%d tests, %d failures\n", nr_tests, nr_tests - nr_passed);
+
+    return nr_passed == nr_tests ? 0 : 1;
+}

config-x86-common.mak
@@ -92,6 +92,8 @@ $(TEST_DIR)/pmu.elf: $(cstart.o) $(TEST_DIR)/pmu.o
 
 $(TEST_DIR)/asyncpf.elf: $(cstart.o) $(TEST_DIR)/asyncpf.o
 
+$(TEST_DIR)/pcid.elf: $(cstart.o) $(TEST_DIR)/pcid.o
+
 arch_clean:
 	$(RM) $(TEST_DIR)/*.o $(TEST_DIR)/*.flat $(TEST_DIR)/*.elf \
 	$(TEST_DIR)/.*.d $(TEST_DIR)/lib/.*.d $(TEST_DIR)/lib/*.o

config-x86_64.mak
@@ -6,7 +6,8 @@ CFLAGS += -D__x86_64__
 
 tests = $(TEST_DIR)/access.flat $(TEST_DIR)/apic.flat \
 	  $(TEST_DIR)/emulator.flat $(TEST_DIR)/idt_test.flat \
-	  $(TEST_DIR)/xsave.flat $(TEST_DIR)/rmap_chain.flat
+	  $(TEST_DIR)/xsave.flat $(TEST_DIR)/rmap_chain.flat \
+	  $(TEST_DIR)/pcid.flat
 tests += $(TEST_DIR)/svm.flat
 
 include config-x86-common.mak

x86/README
@@ -13,3 +13,4 @@ smptest: run smp_id() on every cpu and compares return value to number
 tsc: write to tsc(0) and write to tsc(100000000000) and read it back
 vmexit: long loops for each: cpuid, vmcall, mov_from_cr8, mov_to_cr8, inl_pmtimer, ipi, ipi+halt
 kvmclock_test: test of wallclock, monotonic cycle and performance of kvmclock
+pcid: basic functionality test of PCID/INVPCID feature

x86/unittests.cfg
@@ -98,3 +98,7 @@ extra_params = -cpu qemu64,-svm
 file = kvmclock_test.flat
 smp = 2
 extra_params = --append "10000000 `date +%s`"
+
+[pcid]
+file = pcid.flat
+extra_params = -cpu qemu64,+pcid

commit fb0e760a606780e7ae677cfa4be4b461ce7bf782

x86: Assign a canonical address before execute invpcid
Accidently we see pcid test failed as INVPCID_DESC[127:64] is
uninitialized before execute invpcid.

According to Intel spec: "#GP If INVPCID_TYPE is 0 and the linear
address in INVPCID_DESC[127:64] is not canonical."

By zeroing the whole invpcid_desc structure, ensure the address
canonical and reserved bit zero in desc.

Also change invpcid_desc to be clearly 128bit in size no matter
in 64bit or 32bit mode to match the description in spec.

Fixes: b44d84dae10c ("Add PCID/INVPCID test")
Suggested-by: Sean Christopherson <seanjc@google.com>
Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
Reviewed-by: Sean Christopherson <seanjc@google.com>
Message-Id: <20220117084618.442906-1-zhenzhong.duan@intel.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/pcid.c
@@ -5,9 +5,9 @@
 #include "desc.h"
 
 struct invpcid_desc {
-    unsigned long pcid : 12;
-    unsigned long rsv  : 52;
-    unsigned long addr : 64;
+    u64 pcid : 12;
+    u64 rsv  : 52;
+    u64 addr : 64;
 };
 
 static int write_cr0_checking(unsigned long val)
@@ -73,12 +73,12 @@ static void test_invpcid_enabled(int pcid_enabled)
     int passed = 0, i;
     ulong cr4 = read_cr4();
     struct invpcid_desc desc;
-    desc.rsv = 0;
+
+    memset(&desc, 0, sizeof(desc));
 
     /* try executing invpcid when CR4.PCIDE=0, desc.pcid=0 and type=0..3
      * no exception expected
      */
-    desc.pcid = 0;
     for (i = 0; i < 4; i++) {
         if (invpcid_checking(i, &desc) != 0)
             goto report;