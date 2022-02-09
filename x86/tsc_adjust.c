kvm-unit-tests/kvm-unit-tests

commit 5fecf5d8cad101e2a3ce7af85ed18bc44624e552

Added tests for ia32_tsc_adjust funtionality.
Added x86/tsc_adjust.c and updated x86/vmexit.c to include timing tests
for reading and writing the emulated IA32_TSC_ADJUST msr.

Signed-off-by: Will Auld <will.auld@intel.com>
Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

x86/tsc_adjust.c
@@ -1,0 +1,60 @@
+#include "libcflat.h"
+#include "processor.h"
+
+#define IA32_TSC_ADJUST 0x3b
+
+int main()
+{
+	u64 t1, t2, t3, t4, t5;
+	u64 est_delta_time;
+	bool pass = true;
+
+	if (cpuid(7).b & (1 << 1)) { // IA32_TSC_ADJUST Feature is enabled?
+		if ( rdmsr(IA32_TSC_ADJUST) != 0x0) {
+			printf("failure: IA32_TSC_ADJUST msr was incorrectly"
+				" initialized\n");
+			pass = false;
+		}
+		t3 = 100000000000ull;
+		t1 = rdtsc();
+		wrmsr(IA32_TSC_ADJUST, t3);
+		t2 = rdtsc();
+		if (rdmsr(IA32_TSC_ADJUST) != t3) {
+			printf("failure: IA32_TSC_ADJUST msr read / write"
+				" incorrect\n");
+			pass = false;
+		}
+		if (t2 - t1 < t3) {
+			printf("failure: TSC did not adjust for IA32_TSC_ADJUST"
+				" value\n");
+			pass = false;
+		}
+		t3 = 0x0;
+		wrmsr(IA32_TSC_ADJUST, t3);
+		if (rdmsr(IA32_TSC_ADJUST) != t3) {
+			printf("failure: IA32_TSC_ADJUST msr read / write"
+				" incorrect\n");
+			pass = false;
+		}
+		t4 = 100000000000ull;
+		t1 = rdtsc();
+		wrtsc(t4);
+		t2 = rdtsc();
+		t5 = rdmsr(IA32_TSC_ADJUST);
+		// est of time between reading tsc and writing tsc,
+		// (based on IA32_TSC_ADJUST msr value) should be small
+		est_delta_time = t4 - t5 - t1;
+		if (est_delta_time > 2 * (t2 - t4)) {
+			// arbitray 2x latency (wrtsc->rdtsc) threshold
+			printf("failure: IA32_TSC_ADJUST msr incorrectly"
+				" adjusted on tsc write\n");
+			pass = false;
+		}
+		if (pass) printf("success: IA32_TSC_ADJUST enabled and"
+				" working correctly\n");
+	}
+	else {
+		printf("success: IA32_TSC_ADJUST feature not enabled\n");
+	}
+	return pass?0:1;
+}

config-x86-common.mak
@@ -34,7 +34,8 @@ tests-common = $(TEST_DIR)/vmexit.flat $(TEST_DIR)/tsc.flat \
                $(TEST_DIR)/realmode.flat $(TEST_DIR)/msr.flat \
                $(TEST_DIR)/hypercall.flat $(TEST_DIR)/sieve.flat \
                $(TEST_DIR)/kvmclock_test.flat  $(TEST_DIR)/eventinj.flat \
-               $(TEST_DIR)/s3.flat $(TEST_DIR)/pmu.flat $(TEST_DIR)/asyncpf.flat
+               $(TEST_DIR)/s3.flat $(TEST_DIR)/pmu.flat \
+               $(TEST_DIR)/tsc_adjust.flat $(TEST_DIR)/asyncpf.flat
 
 ifdef API
 tests-common += api/api-sample
@@ -63,6 +64,8 @@ $(TEST_DIR)/emulator.elf: $(cstart.o) $(TEST_DIR)/emulator.o
 $(TEST_DIR)/port80.elf: $(cstart.o) $(TEST_DIR)/port80.o
 
 $(TEST_DIR)/tsc.elf: $(cstart.o) $(TEST_DIR)/tsc.o
+
+$(TEST_DIR)/tsc_adjust.elf: $(cstart.o) $(TEST_DIR)/tsc_adjust.o
 
 $(TEST_DIR)/apic.elf: $(cstart.o) $(TEST_DIR)/apic.o
 

x86/vmexit.c
@@ -34,6 +34,7 @@ static void vmcall(void)
 	asm volatile ("vmcall" : "+a"(a), "=b"(b), "=c"(c), "=d"(d));
 }
 
+#define MSR_TSC_ADJUST 0x3b
 #define MSR_EFER 0xc0000080
 #define EFER_NX_MASK            (1ull << 11)
 
@@ -103,6 +104,16 @@ static void ple_round_robin(void)
 	++counters[you].n1;
 }
 
+static void rd_tsc_adjust_msr(void)
+{
+	rdmsr(MSR_TSC_ADJUST);
+}
+
+static void wr_tsc_adjust_msr(void)
+{
+	wrmsr(MSR_TSC_ADJUST, 0x0);
+}
+
 static struct test {
 	void (*func)(void);
 	const char *name;
@@ -119,6 +130,8 @@ static struct test {
 	{ ipi, "ipi", is_smp, .parallel = 0, },
 	{ ipi_halt, "ipi+halt", is_smp, .parallel = 0, },
 	{ ple_round_robin, "ple-round-robin", .parallel = 1 },
+	{ wr_tsc_adjust_msr, "wr_tsc_adjust_msr", .parallel = 1 },
+	{ rd_tsc_adjust_msr, "rd_tsc_adjust_msr", .parallel = 1 },
 };
 
 unsigned iterations;

commit d3f3e2151bb3ee3e41f76fd5db5ad182454bf3d7

x86: tsc_adjust: Remove flaky timing test
The subtest labeled, "MSR_IA32_TSC_ADJUST msr adjustment on tsc
write," sometimes fails. The behavior tested is neither architected
nor guaranteed. Running under qemu/kvm, the 'est_delta_time' has been
observed to be as much as an order of magnitude greater than the
expression that is supposed to be its upper bound.

Remove the flaky subtest, and replace it with some invariants that
actually are architecturally guaranteed (as long as IA32_TSC doesn't
wrap around).

Fixes: 5fecf5d8cad1 ("Added tests for ia32_tsc_adjust funtionality.")
Cc: Will Auld <will.auld.intel@gmail.com>
Signed-off-by: Jim Mattson <jmattson@google.com>
Message-Id: <20220127215548.2016946-1-jmattson@google.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/tsc_adjust.c
@@ -4,7 +4,6 @@
 int main(void)
 {
 	u64 t1, t2, t3, t4, t5;
-	u64 est_delta_time;
 
 	if (this_cpu_has(X86_FEATURE_TSC_ADJUST)) { // MSR_IA32_TSC_ADJUST Feature is enabled?
 		report(rdmsr(MSR_IA32_TSC_ADJUST) == 0x0,
@@ -26,12 +25,9 @@ int main(void)
 		wrtsc(t4);
 		t2 = rdtsc();
 		t5 = rdmsr(MSR_IA32_TSC_ADJUST);
-		// est of time between reading tsc and writing tsc,
-		// (based on MSR_IA32_TSC_ADJUST msr value) should be small
-		est_delta_time = t4 - t5 - t1;
-		// arbitray 2x latency (wrtsc->rdtsc) threshold
-		report(est_delta_time <= (2 * (t2 - t4)),
-		       "MSR_IA32_TSC_ADJUST msr adjustment on tsc write");
+		report(t1 <= t4 - t5,
+		       "Internal TSC advances across write to IA32_TSC");
+		report(t2 >= t4, "IA32_TSC advances after write to IA32_TSC");
 	}
 	else {
 		report_pass("MSR_IA32_TSC_ADJUST feature not enabled");