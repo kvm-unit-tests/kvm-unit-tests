kvm-unit-tests/kvm-unit-tests

commit 965e38a097b2ed0676690a5fa1f2cb94fd9a58c5

s390x: Test effect of storage keys on diag 308
Test that key-controlled protection does not apply to diag 308.

Signed-off-by: Janis Schoetterl-Glausch <scgl@linux.ibm.com>
Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
Message-Id: <20220621143609.753452-4-scgl@linux.ibm.com>
Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>

s390x/skey.c
@@ -303,6 +303,31 @@ static void test_store_cpu_address(void)
 	report_prefix_pop();
 }
 
+static void test_diag_308(void)
+{
+	uint16_t response;
+	uint32_t *ipib = (uint32_t *)pagebuf;
+
+	report_prefix_push("DIAG 308");
+	WRITE_ONCE(ipib[0], 0); /* Invalid length */
+	set_storage_key(ipib, 0x28, 0);
+	/* key-controlled protection does not apply */
+	asm volatile (
+		"lr	%%r2,%[ipib]\n\t"
+		"spka	0x10\n\t"
+		"diag	%%r2,%[code],0x308\n\t"
+		"spka	0\n\t"
+		"lr	%[response],%%r3\n"
+		: [response] "=d" (response)
+		: [ipib] "d" (ipib),
+		  [code] "d" (5)
+		: "%r2", "%r3"
+	);
+	report(response == 0x402, "no exception on fetch, response: invalid IPIB");
+	set_storage_key(ipib, 0x00, 0);
+	report_prefix_pop();
+}
+
 /*
  * Perform CHANNEL SUBSYSTEM CALL (CHSC)  instruction while temporarily executing
  * with access key 1.
@@ -715,6 +740,7 @@ int main(void)
 	test_chg();
 	test_test_protection();
 	test_store_cpu_address();
+	test_diag_308();
 	test_channel_subsystem_call();
 
 	setup_vm();

commit e11a0e2f881d7bc038f44d8d4f99b2d55a01bc4e

s390x: Fix integer literal in skey.c
The code is a 64bit number of which the upper 48 bits must be 0.

Fixes: 965e38a0 ("s390x: Test effect of storage keys on diag 308")
Signed-off-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
Message-Id: <20230104175950.731988-1-nsg@linux.ibm.com>
Signed-off-by: Thomas Huth <thuth@redhat.com>

s390x/skey.c
@@ -320,7 +320,7 @@ static void test_diag_308(void)
 		"lr	%[response],%%r3\n"
 		: [response] "=d" (response)
 		: [ipib] "d" (ipib),
-		  [code] "d" (5)
+		  [code] "d" (5L)
 		: "%r2", "%r3"
 	);
 	report(response == 0x402, "no exception on fetch, response: invalid IPIB");