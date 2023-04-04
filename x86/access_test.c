88c78d29f21431f55e4a940e1bf11b036736c152
Sean Christopherson <seanjc@google.com>
2023-04-04 09:53:34 -0700

    x86/access: Replace spaces with tabs in access_test.c
    Replace spaces with tabs in the access test wrapper to adhere to the
    preferred style.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20230404165341.163500-4-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- x86/access_test.c
+++ x86/access_test.c
@@ -5,22 +5,22 @@
 
 int main(void)
 {
-    int r;
+	int r;
 
-    printf("starting test\n\n");
-    r = ac_test_run(PT_LEVEL_PML4);
+	printf("starting test\n\n");
+	r = ac_test_run(PT_LEVEL_PML4);
 
 #ifndef CONFIG_EFI
-    /*
-     * Not supported yet for UEFI, because setting up 5
-     * level page table requires entering real mode.
-     */
-    if (this_cpu_has(X86_FEATURE_LA57)) {
-        printf("starting 5-level paging test.\n\n");
-        setup_5level_page_table();
-        r = ac_test_run(PT_LEVEL_PML5);
-    }
+	/*
+	* Not supported yet for UEFI, because setting up 5
+	* level page table requires entering real mode.
+	*/
+	if (this_cpu_has(X86_FEATURE_LA57)) {
+		printf("starting 5-level paging test.\n\n");
+		setup_5level_page_table();
+		r = ac_test_run(PT_LEVEL_PML5);
+	}
 #endif
 
-    return r ? 0 : 1;
+	return r ? 0 : 1;
 }
