533d7781753e830d7675d1d4e61c2e49ba9fefcf
Nicholas Piggin <npiggin@gmail.com>
2024-04-06 22:38:22 +1000

    shellcheck: Fix SC2048
      SC2048 (warning): Use "$@" (with quotes) to prevent whitespace
      problems.
    
    No bug identified.
    
    Reviewed-by: Andrew Jones <andrew.jones@linux.dev>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Message-ID: <20240406123833.406488-14-npiggin@gmail.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- run_tests.sh
+++ run_tests.sh
@@ -44,7 +44,7 @@ fi
 
 only_tests=""
 list_tests=""
-args=$(getopt -u -o ag:htj:vl -l all,group:,help,tap13,parallel:,verbose,list,probe-maxsmp -- $*)
+args=$(getopt -u -o ag:htj:vl -l all,group:,help,tap13,parallel:,verbose,list,probe-maxsmp -- "$@")
 [ $? -ne 0 ] && exit 2;
 set -- $args;
 while [ $# -gt 0 ]; do
