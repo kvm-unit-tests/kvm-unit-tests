kvm-unit-tests/kvm-unit-tests

commit 6895ce6dc618079972b9b60be6aeccdcff28cb40

riscv: Populate memregions and switch to page allocator
Get the memory regions from the DT rather than just assuming we have
enough. Getting memory regions and setting their flags is also a
first step to enabling the MMU. Also switch to the page allocator,
which is a second step to enabling the MMU.

Signed-off-by: Andrew Jones <andrew.jones@linux.dev>
Acked-by: Thomas Huth <thuth@redhat.com>

lib/riscv/setup.c
@@ -6,21 +6,30 @@
  */
 #include <libcflat.h>
 #include <alloc.h>
+#include <alloc_page.h>
 #include <alloc_phys.h>
 #include <argv.h>
 #include <cpumask.h>
 #include <devicetree.h>
+#include <memregions.h>
 #include <on-cpus.h>
 #include <asm/csr.h>
 #include <asm/page.h>
 #include <asm/processor.h>
 #include <asm/setup.h>
 
+#define VA_BASE			((phys_addr_t)3 * SZ_1G)
+
+#define MAX_DT_MEM_REGIONS	16
+#define NR_MEM_REGIONS		(MAX_DT_MEM_REGIONS + 16)
+
 char *initrd;
 u32 initrd_size;
 
 struct thread_info cpus[NR_CPUS];
 int nr_cpus;
+
+static struct mem_region riscv_mem_regions[NR_MEM_REGIONS + 1];
 
 int hartid_to_cpu(unsigned long hartid)
 {
@@ -64,10 +73,50 @@ static void cpu_init(void)
 	cpu0_calls_idle = true;
 }
 
+extern unsigned long _etext;
+
 static void mem_init(phys_addr_t freemem_start)
 {
-	//TODO - for now just assume we've got some memory available
-	phys_alloc_init(freemem_start, 16 * SZ_1M);
+	struct mem_region *freemem, *code, *data;
+	phys_addr_t freemem_end, base, top;
+
+	memregions_init(riscv_mem_regions, NR_MEM_REGIONS);
+	memregions_add_dt_regions(MAX_DT_MEM_REGIONS);
+
+	/* Split the region with the code into two regions; code and data */
+	memregions_split((unsigned long)&_etext, &code, &data);
+	assert(code);
+	code->flags |= MR_F_CODE;
+
+	freemem = memregions_find(freemem_start);
+	assert(freemem && !(freemem->flags & (MR_F_IO | MR_F_CODE)));
+
+	freemem_end = freemem->end & PAGE_MASK;
+
+	/*
+	 * The assert below is mostly checking that the free memory doesn't
+	 * start in the 3G-4G range, which is reserved for virtual addresses,
+	 * but it also confirms that there is some free memory (the amount
+	 * is arbitrarily selected, but should be sufficient for a unit test)
+	 *
+	 * TODO: Allow the VA range to shrink and move.
+	 */
+	if (freemem_end > VA_BASE)
+		freemem_end = VA_BASE;
+	assert(freemem_end - freemem_start >= SZ_1M * 16);
+
+	/*
+	 * TODO: Remove the need for this phys allocator dance, since, as we
+	 * can see with the assert, we could have gone straight to the page
+	 * allocator.
+	 */
+	phys_alloc_init(freemem_start, freemem_end - freemem_start);
+	phys_alloc_set_minimum_alignment(PAGE_SIZE);
+	phys_alloc_get_unused(&base, &top);
+	assert(base == freemem_start && top == freemem_end);
+
+	page_alloc_init_area(0, freemem_start >> PAGE_SHIFT, freemem_end >> PAGE_SHIFT);
+	page_alloc_ops_enable();
 }
 
 static void banner(void)
@@ -86,7 +135,7 @@ void setup(const void *fdt, phys_addr_t freemem_start)
 	u32 fdt_size;
 	int ret;
 
-	assert(sizeof(long) == 8 || freemem_start < (3ul << 30));
+	assert(sizeof(long) == 8 || freemem_start < VA_BASE);
 	freemem = (void *)(unsigned long)freemem_start;
 
 	/* Move the FDT to the base of free memory */

lib/riscv/asm/memory_areas.h
@@ -1,0 +1,1 @@
+#include <asm-generic/memory_areas.h>

riscv/Makefile
@@ -22,8 +22,10 @@ $(TEST_DIR)/sieve.elf: AUXFLAGS = 0x1
 cstart.o = $(TEST_DIR)/cstart.o
 
 cflatobjs += lib/alloc.o
+cflatobjs += lib/alloc_page.o
 cflatobjs += lib/alloc_phys.o
 cflatobjs += lib/devicetree.o
+cflatobjs += lib/memregions.o
 cflatobjs += lib/on-cpus.o
 cflatobjs += lib/riscv/bitops.o
 cflatobjs += lib/riscv/io.o

commit 7449271742ff86a123428323db6b252240997889

riscv: setup: Apply VA_BASE check to rv64
The VA_BASE check in setup() also applies to rv64, as is clear from
the later VA_BASE check in mem_allocator_init(), which ensures
freemem_start < freemem_end < VA_BASE.

Fixes: 6895ce6dc618 ("riscv: Populate memregions and switch to page allocator")
Signed-off-by: Andrew Jones <andrew.jones@linux.dev>

lib/riscv/setup.c
@@ -193,7 +193,7 @@ void setup(const void *fdt, phys_addr_t freemem_start)
 	const char *bootargs;
 	int ret;
 
-	assert(sizeof(long) == 8 || freemem_start < VA_BASE);
+	assert(freemem_start < VA_BASE);
 	freemem = __va(freemem_start);
 
 	freemem_push_fdt(&freemem, fdt);