81dcf3f7d56899097e4cfd20e88a2a160c85a8c3
Sean Christopherson <seanjc@google.com>
2025-02-14 17:30:16 -0800

    x86: Expand LA57 test to 64-bit mode (to prep for canonical testing)
    Expand the LA57 test to also validate that attempting to toggle LA57 while
    in 64-bit mode fails.  The extra test coverage isn't terribly interesting,
    but it's still useful, and extending the LA57 test to 64-bit configs will
    allow extending it further with LA57-specific canonical tests.
    
    Link: https://lore.kernel.org/r/20250215013018.1210432-5-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- x86/la57.c
+++ x86/la57.c
@@ -5,9 +5,12 @@
 int main(int ac, char **av)
 {
 	int vector = write_cr4_safe(read_cr4() | X86_CR4_LA57);
-	int expected = this_cpu_has(X86_FEATURE_LA57) ? 0 : 13;
+	bool is_64bit = rdmsr(MSR_EFER) & EFER_LMA;
+	int expected = !is_64bit && this_cpu_has(X86_FEATURE_LA57) ? 0 : GP_VECTOR;
 
-	report(vector == expected, "%s when CR4.LA57 %ssupported",
-	       expected ? "#GP" : "No fault", expected ? "un" : "");
+	report(vector == expected, "%s when CR4.LA57 %ssupported (in %u-bit mode)",
+	       expected ? "#GP" : "No fault",
+	       this_cpu_has(X86_FEATURE_LA57) ? "un" : "", is_64bit ? 64 : 32);
+
 	return report_summary();
 }
