985705608940405c791f033121a0bb35449d9f56
Alexandru Elisei <alexandru.elisei@arm.com>
2025-06-25 16:48:01 +0100

    run_tests.sh: Document --probe-maxsmp argument
    Commit 5dd20ec76ea63 ("runtime: Update MAX_SMP probe") added the
    --probe-maxmp argument, but the help message for run_tests.sh wasn't
    updated. Document --probe-maxsmp.
    
    Reviewed-by: Andrew Jones <andrew.jones@linux.dev>
    Reviewed-by: Shaoqin Huang <shahuang@redhat.com>
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>
    Signed-off-by: Andrew Jones <andrew.jones@linux.dev>

--- run_tests.sh
+++ run_tests.sh
@@ -17,14 +17,15 @@ cat <<EOF
 
 Usage: $0 [-h] [-v] [-a] [-g group] [-j NUM-TASKS] [-t] [-l]
 
-    -h, --help      Output this help text
-    -v, --verbose   Enables verbose mode
-    -a, --all       Run all tests, including those flagged as 'nodefault'
-                    and those guarded by errata.
-    -g, --group     Only execute tests in the given group
-    -j, --parallel  Execute tests in parallel
-    -t, --tap13     Output test results in TAP format
-    -l, --list      Only output all tests list
+    -h, --help          Output this help text
+    -v, --verbose       Enables verbose mode
+    -a, --all           Run all tests, including those flagged as 'nodefault'
+                        and those guarded by errata.
+    -g, --group         Only execute tests in the given group
+    -j, --parallel      Execute tests in parallel
+    -t, --tap13         Output test results in TAP format
+    -l, --list          Only output all tests list
+        --probe-maxsmp  Update the maximum number of VCPUs supported by host
 
 Set the environment variable QEMU=/path/to/qemu-system-ARCH to
 specify the appropriate qemu binary for ARCH-run.
