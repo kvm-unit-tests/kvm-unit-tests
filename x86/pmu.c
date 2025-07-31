kvm-unit-tests/kvm-unit-tests

commit 92dc5f7ab45914db6141f2a89fd52c533ff89aaf

x86/pmu: Mark Intel architectural event available iff X <= CPUID.0xA.EAX[31:24]
Mask the set of available architectural events based on the bit vector
length to avoid marking reserved/undefined events as available.  Per the
SDM:

  EAX Bits 31-24: Length of EBX bit vector to enumerate architectural
                  performance monitoring events. Architectural event x is
                  supported if EBX[x]=0 && EAX[31:24]>x.

Suggested-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
Reviewed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
Link: https://lore.kernel.org/r/20250610195415.115404-8-seanjc@google.com
Signed-off-by: Sean Christopherson <seanjc@google.com>

x86/pmu.c
@@ -993,6 +993,7 @@ int main(int ac, char **av)
 	printf("GP counters:         %d\n", pmu.nr_gp_counters);
 	printf("GP counter width:    %d\n", pmu.gp_counter_width);
 	printf("Event Mask length:   %d\n", pmu.arch_event_mask_length);
+	printf("Arch Events (mask):  0x%x\n", pmu.arch_event_available);
 	printf("Fixed counters:      %d\n", pmu.nr_fixed_counters);
 	printf("Fixed counter width: %d\n", pmu.fixed_counter_width);
 

lib/x86/pmu.c
@@ -21,7 +21,8 @@ void pmu_init(void)
 		pmu.arch_event_mask_length = (cpuid_10.a >> 24) & 0xff;
 
 		/* CPUID.0xA.EBX bit is '1' if an arch event is NOT available. */
-		pmu.arch_event_available = ~cpuid_10.b;
+		pmu.arch_event_available = ~cpuid_10.b &
+					   (BIT(pmu.arch_event_mask_length) - 1);
 
 		if (this_cpu_has(X86_FEATURE_PDCM))
 			pmu.perf_cap = rdmsr(MSR_IA32_PERF_CAPABILITIES);

commit 989726f28e1e2137a5cafbe187fffc335cc15b2a

x86/pmu: Fix compilation on macOS
Compiling for i686 on macOS in the CI currently fails with:

.../cirrus-ci-build/x86/pmu.c: In function 'main':
.../cirrus-ci-build/x86/pmu.c:1012:41: error: format '%x' expects argument of
 type 'unsigned int', but argument 2 has type 'u32' {aka 'long unsigned int'}
 [-Werror=format=]
 1012 |     printf("Arch Events (mask):  0x%x\n", pmu.arch_event_available);
      |                                    ~^     ~~~~~~~~~~~~~~~~~~~~~~~~
      |                                     |        |
      |                                     |        u32 {aka long unsigned int}
      |                                     unsigned int
      |                                    %lx

Use the correct format string for u32 to fix this issue.

Fixes: 92dc5f7a ("x86/pmu: Mark Intel architectural event available [...]")
Signed-off-by: Thomas Huth <thuth@redhat.com>
Message-ID: <20250723164742.1174289-1-thuth@redhat.com>

x86/pmu.c
@@ -1009,7 +1009,7 @@ int main(int ac, char **av)
 	printf("GP counters:         %d\n", pmu.nr_gp_counters);
 	printf("GP counter width:    %d\n", pmu.gp_counter_width);
 	printf("Event Mask length:   %d\n", pmu.arch_event_mask_length);
-	printf("Arch Events (mask):  0x%x\n", pmu.arch_event_available);
+	printf("Arch Events (mask):  0x%" PRIx32 "\n", pmu.arch_event_available);
 	printf("Fixed counters:      %d\n", pmu.nr_fixed_counters);
 	printf("Fixed counter width: %d\n", pmu.fixed_counter_width);
 